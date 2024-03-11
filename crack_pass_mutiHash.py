c208e4749ae673ecedc36264db27a2240d6a9c50
thanawee321 <53590032+thanawee321@users.noreply.github.com>
2024-03-11 08:54:30 +0700

    Add files via upload

--- crack_pass_mutiHash.py
+++ crack_pass_mutiHash.py
@@ -1,16 +1,17 @@
 import hashlib
-from tqdm import tqdm  # เรียกใช้ tqdm เพื่อแสดงแถบความคืบหน้า
+from tqdm import tqdm  
 import os
 import pyqrcode
+import pyperclip
 
-# กำหนดรายการเพื่อเก็บรหัสผ่าน
+
+
 list_password = []
 
-# กำหนดฟล็อกเพื่อตรวจสอบว่าพบรหัสผ่านหรือไม่
 
-# ฟังก์ชันเพื่อแสดงตัวเลือกรายการเมนูโดยใช้ข้อมูลเมนูเข้ารหัส
+
 def type_list(menu):
-    # กำหนดการแมปหมายเลขเมนูเพื่อประเภทการเข้ารหัสแต่ละชนิด
+    
     if menu == '1':
         return "-- MD5 HASH --"
     if menu == '2':
@@ -22,236 +23,168 @@ def type_list(menu):
     if menu == '5':
         return "-- SHA512 HASH --"
 
-# ฟังก์ชันเพื่อแปลงข้อความรหัสผ่านเป็น MD5 hash
+
+
 def convert_text_to_md5(text_password):
+    
     digest = hashlib.md5(text_password.encode()).hexdigest()
     return digest
 
-# ฟังก์ชันเพื่อแปลงข้อความรหัสผ่านเป็นรหัส hash SHA1
 def convert_text_to_sha1(text_password):
+    
     digest = hashlib.sha1(text_password.encode()).hexdigest()
     return digest
 
-# ฟังก์ชันเพื่อแปลงข้อความรหัสผ่านเป็นรหัส hash SHA256
 def convert_text_to_sha256(text_password):
+    
     digest = hashlib.sha256(text_password.encode()).hexdigest()
     return digest
 
-# ฟังก์ชันเพื่อแปลงข้อความรหัสผ่านเป็นรหัส hash SHA384
 def convert_text_to_sha384(text_password):
+    
     digest = hashlib.sha384(text_password.encode()).hexdigest()
     return digest
 
-# ฟังก์ชันเพื่อแปลงข้อความรหัสผ่านเป็นรหัส hash SHA512
 def convert_text_to_sha512(text_password):
+    
     digest = hashlib.sha512(text_password.encode()).hexdigest()
     return digest
 
 
 
-# ฟังก์ชันเพื่ออ่านข้อมูลรหัสผ่านจากไฟล์
 def get_data_password():
+    
     file = input("Setup the list file password : ")
     try:
+        #with open('rockyou.txt', encoding='utf-8') as f:
         with open(file, encoding='utf-8') as f:
-            # ใช้ tqdm เพื่อแสดงแถบความคืบหน้าในขณะอ่านบรรทัดจากไฟล์
+            
             for i in tqdm(f, desc="Loading...", ascii=False, ncols=100):
                 list_password.append(i.strip())
+                
     except Exception as e:
         print("[-] File not found or Wrong location source!!")
         print(f"[!] {e}\n")
+        
     except KeyboardInterrupt:
         print("[+] Exit to Program")
+        
     return list_password
 
-# ฟังก์ชันหลักเพื่อทำการเปรียบเทียบรหัสผ่าน
-def main(menu):
+
+
+def create_hash():
+    
     try:
         while True:
-            os.system('cls')  # เคลียร์หน้าจอ
-            print(type_list(menu))
-            user_hash = input("Enter your hash: ").strip().lower()
-
-            # ลูปผ่านรายการรหัสผ่าน
-            for line in list_password:
-                
-                # ตรรจสอบเหมือนกันสำหรับชนิดของ hash อื่น ๆ
-                # คุณสามารถทำซ้ำบล็อค if ด้านบนสำหรับ SHA1, SHA256, SHA384, SHA512
-                if menu == '1':
-                    convert_hash = convert_text_to_md5(line)
-                    if convert_hash == user_hash:
-                        print("-------------------------------------------------------")
-                        print("[+] [Status] -- Password Found -- ")
-                        print(f"[+] This is a Password = {line}")
-                        os.system('pause')
-                        break
-                    
-                if menu == '2':
-                    convert_hash = convert_text_to_sha1(line)
-                    if convert_hash == user_hash:
-                        print("-------------------------------------------------------")
-                        print("[+] [Status] -- Password Found -- ")
-                        print(f"[+] This is a Password = {line}")
-                        os.system('pause')
-                        break
-                
-                if menu == '3':
-                    convert_hash = convert_text_to_sha256(line)
-                    if convert_hash == user_hash:
-                        print("-------------------------------------------------------")
-                        print("[+] [Status] -- Password Found -- ")
-                        print(f"[+] This is a Password = {line}")
-                        os.system('pause')
-                        break
-                
-                
-                if menu == '4':
-                    convert_hash = convert_text_to_sha384(line)
-                    if convert_hash == user_hash:
-                        print("-------------------------------------------------------")
-                        print("[+] [Status] -- Password Found -- ")
-                        print(f"[+] This is a Password = {line}") 
-                        os.system('pause')
-                        break
-                    
-                if menu == '5':
-                    convert_hash = convert_text_to_sha512(line)
-                    if convert_hash == user_hash:
-                        print("-------------------------------------------------------")
-                        print("[+] [Status] -- Password Found -- ")
-                        print(f"[+] This is a Password = {line}") 
-                        os.system('pause')
-                        break
-
-            else:
-                print("[-] [Status] -- Password NOT Found --")
-                print("[!] Please check your hash again")
-                os.system('pause')  # หยุดการดำเนินการหากรหัสผ่านไม่พบ
-
-    except KeyboardInterrupt:
-        os.system('cls')  # เคลียร์หน้าจอ
-        print("Back to Main menu")
-
-# ฟังก์ชันเพื่อจัดการการเลือกเมนูและการนำทาง
-if __name__ == '__main__':
-    get_data_password() # โหลดข้อมูลรหัสผ่านจากไฟล์
-    os.system('pause')
-    os.system('cls')
-    try:
-        while True:
-            print("\n|-- Welcome to Program HASH Convert!!! --|")
-            print("           Dev by BabyH@ck V.3.7.9\n")
+            print("-- Create HASH --")
             print("[1] MD5 HASH")
             print("[2] SHA1 HASH")
             print("[3] SHA256 HASH")
             print("[4] SHA384 HASH")
             print("[5] SHA512 HASH")
-            print("\n[99] Setup source file Password")
+
+            you_hash = input("\n[+] Insert you string : ")
+            create = input("[+] Select to hash mode : ")
             
-            print("\n*** Do you want to help menu,Please Enter Help ***")
-            print("-------------------------------------------------------")
-            menu = str(input("Select menu : "))
-            if menu in ['1', '2', '3', '4', '5']:
-                os.system('cls')  # เคลียร์หน้าจอ
-                main(menu)  # เรียกใช้ฟังก์ชันหลักตามที่เลือกในเมนู
-                
-            elif menu == 'help' or menu == 'Help':
-                os.system('cls')
-                print("** Help Menu *** ")
-                print("Ctrl+C : Exit to menu/Exit to program")
-                print("MD5 HASH : is a menu hash decrypt")
-                print("SHA1 HASH : is a menu hash decrypt")
-                print("SHA256 HASH : is a menu hash decrypt")
-                print("SHA384 HASH : is a menu hash decrypt")
-                print("SHA512 HASH : is a menu hash decrypt")
-                print("Setup source file Password : is a setup source file password,Do you want to change list file password")
+            if create == '1':
+               pyperclip.copy(convert_text_to_md5(you_hash))
+               this_hash = pyperclip.paste()
+               print("-------------------------------------------------------")
+               print("[+] [Status] -- Generate Sucessfully -- ")
+               print(f"[+] This HASH = {this_hash}")
+               os.system('pause')
+               os.system('cls')
+               break
+           
+            elif create == '2':
+                pyperclip.copy(convert_text_to_sha1(you_hash))
+                this_hash = pyperclip.paste()
+                print("-------------------------------------------------------")
+                print("[+] [Status] -- Generate Sucessfully -- ")
+                print(f"[+] This HASH = {this_hash}")
                 os.system('pause')
                 os.system('cls')
+                break
+            
+            elif create == '3':
+                pyperclip.copy(convert_text_to_sha256(you_hash))
+                this_hash = pyperclip.paste()
+                print("-------------------------------------------------------")
+                print("[+] [Status] -- Generate Sucessfully -- ")
+                print(f"[+] This HASH = {this_hash}")
+                os.system('pause')
+                os.system('cls')
+                break
+            
+            elif create == '4':
+                pyperclip.copy(convert_text_to_sha384(you_hash))
+                this_hash = pyperclip.paste()
+                print("-------------------------------------------------------")
+                print("[+] [Status] -- Generate Sucessfully -- ")
+                print(f"[+] This HASH = {this_hash}")
+                os.system('pause')
+                os.system('cls')
+                break
                 
-            elif menu == '99':
+            elif create == '5':
+                pyperclip.copy(convert_text_to_sha512(you_hash))
+                this_hash = pyperclip.paste()
+                print("-------------------------------------------------------")
+                print("[+] [Status] -- Generate Sucessfully -- ")
+                print(f"[+] This HASH = {this_hash}")
+                os.system('pause')
+                os.system('cls')
+                break
+
+            else:
+                print("[!] Please Select menu again..")
+                break
+            
+    except KeyboardInterrupt:
+        os.system('cls')
+        print("\n [!] Back to main menu")
+    
+    
+
+def advance():
+    
+    try:
+        os.system('cls')
+        
+        while True:
+            print("-- Advance Mode --\n")
+            print("[1] Set up the source file password")
+            print("[2] Create the Hash\n")        
+
+            menuAD = input("[+] Select menu advance : ")
+            if menuAD == '1':
                 os.system('cls')
                 get_data_password()
+                break
+            
+            elif menuAD == '2':
+                os.system('cls')
+                create_hash()
+                break
             else:
-                os.system('cls')  # เคลียร์หน้าจอ
-                print("[!] Select menu again")
+                os.system('cls')
+                print("[!] Please select menu again...\n")
+                
 
     except KeyboardInterrupt:
         os.system('cls')
-        print("\nExit the program")
-        print("-------------------------------------")
-        print("Do you want to Support me? You can sent some donate to me hahaha at a below this message")
-        pay = pyqrcode.create(content="0991626437")
-        print(pay.terminal(module_color='white', background='black'))  
-        os.system('pause')
-
-
-
-#human version
-
-"""import hashlib
-from tqdm import tqdm
-import os
-
-
-list_password = [] 
-found = False 
-
-def type_list(menu):
-    
-    if menu == '1':
-        return "-- MD5 HASH --"
-    if menu == '2':
-        return "-- SHA1 HASH --"
-    if menu == '3':
-        return "-- SHA256 HASH --"
-    if menu == '4':
-        return "-- SHA384 HASH --"
-    if menu == '5':
-        return "-- SHA512 HASH --"
-    
-    
-
- 
-def convert_text_to_md5(text_password):
-    digest = hashlib.md5(text_password.encode()).hexdigest() 
-    return digest  
-
-
-def convert_text_to_sha1(text_password):
-    digest = hashlib.sha1(text_password.encode()).hexdigest()
-    return digest
-
-def convert_text_to_sha256(text_password):
-    digest = hashlib.sha256(text_password.encode()).hexdigest()
-    return digest
-
-def convert_text_to_sha384(text_password):
-    digest = hashlib.sha384(text_password.encode()).hexdigest()
-    return digest
-
-def convert_text_to_sha512(text_password):
-    digest = hashlib.sha512(text_password.encode()).hexdigest()
-    return digest
-
-
-
-
-def get_data_password():
-    with open('./rockyou.txt', encoding='utf-8') as f: 
-        for i in tqdm(f, desc="Loading...", ascii=False, ncols=100):     
-            list_password.append(i.strip())
-    return list_password
-
-
-
+        print("\n [!] Back to main menu")
+        
+        
+        
 def main(menu):
-    
     try:
-        while(True):
-            os.system('cls')
-            
+        while True:
+            os.system('cls')  
             print(type_list(menu))
-            user_hash = input("Enter you hash : ").strip().lower()
+            print("*** Do you want to exit menu,Please Ctrl+c for exit ***\n")
+            user_hash = input("Enter your hash: ").strip().lower()
             
             for line in list_password:
                 
@@ -259,8 +192,8 @@ def main(menu):
                     convert_hash = convert_text_to_md5(line)
                     if convert_hash == user_hash:
                         print("-------------------------------------------------------")
-                        print("[Status] -- Password Found -- ")
-                        print(f"This is a Password = {line}")
+                        print("[+] [Status] -- Password Found -- ")
+                        print(f"[+] This is a Password = {line}")
                         os.system('pause')
                         break
                     
@@ -268,8 +201,8 @@ def main(menu):
                     convert_hash = convert_text_to_sha1(line)
                     if convert_hash == user_hash:
                         print("-------------------------------------------------------")
-                        print("[Status] -- Password Found -- ")
-                        print(f"This is a Password = {line}") 
+                        print("[+] [Status] -- Password Found -- ")
+                        print(f"[+] This is a Password = {line}")
                         os.system('pause')
                         break
                 
@@ -277,18 +210,17 @@ def main(menu):
                     convert_hash = convert_text_to_sha256(line)
                     if convert_hash == user_hash:
                         print("-------------------------------------------------------")
-                        print("[Status] -- Password Found -- ")
-                        print(f"This is a Password = {line}") 
+                        print("[+] [Status] -- Password Found -- ")
+                        print(f"[+] This is a Password = {line}")
                         os.system('pause')
                         break
-                
                 
                 if menu == '4':
                     convert_hash = convert_text_to_sha384(line)
                     if convert_hash == user_hash:
                         print("-------------------------------------------------------")
-                        print("[Status] -- Password Found -- ")
-                        print(f"This is a Password = {line}") 
+                        print("[+] [Status] -- Password Found -- ")
+                        print(f"[+] This is a Password = {line}") 
                         os.system('pause')
                         break
                     
@@ -296,65 +228,75 @@ def main(menu):
                     convert_hash = convert_text_to_sha512(line)
                     if convert_hash == user_hash:
                         print("-------------------------------------------------------")
-                        print("[Status] -- Password Found -- ")
-                        print(f"This is a Password = {line}") 
+                        print("[+] [Status] -- Password Found -- ")
+                        print(f"[+] This is a Password = {line}") 
                         os.system('pause')
                         break
-                
-                
+                    
             else:
-                print("[Status] -- Password NOT Found --")
-                print("Please check you hash agian")
-                os.system('pause')    
-                    
-                    
-                    
+                print("[-] [Status] -- Password NOT Found --")
+                print("[!] Please check your hash again")
+                os.system('pause')  
+
     except KeyboardInterrupt:
-        os.system('cls')
-        print("Back to Main menu")
-    
-    
-    
+        os.system('cls')  
+        print("[!] Back to Main menu")
+        
+        
+        
+def help_menu():
+    print("** Help Menu *** ")
+    print("Ctrl+C : Exit to menu/Exit to program")
+    print("MD5 HASH : is a menu hash decrypt")
+    print("SHA1 HASH : is a menu hash decrypt")
+    print("SHA256 HASH : is a menu hash decrypt")
+    print("SHA384 HASH : is a menu hash decrypt")
+    print("SHA512 HASH : is a menu hash decrypt")
+    print("Setup source file Password : is a setup source file password,Do you want to change list file password")
+
+
 
 if __name__ == '__main__':
-    
-    get_data_password()
+    get_data_password() 
+    os.system('pause')
     os.system('cls')
-    
-   
     try:
-       while(True):
-        print("-- Welcome to Program HASH Convert!!! --") 
-        print("[1] MD5 HASH")
-        print("[2] SHA1 HASH")
-        print("[3] SHA256 HASH")
-        print("[4] SHA384 HASH")
-        print("[5] SHA512 HASH")
         
-        menu = input("Select menu : ")
-        if menu == '1':
-            os.system('cls')
-            main(menu)
-            
-        elif menu == '2':
-            os.system('cls')
-            main(menu)
-            
-        elif menu == '3':
-            os.system('cls')
-            main(menu)
-            
-        elif menu == '4':
-            os.system('cls')
-            main(menu)
-            
-        elif menu == '5':
-            os.system('cls')
-            main(menu)
-            
-        else:
-            os.system('cls')
-            print("Select menu agian")
-        
+        while True:
+            print("\n|-- Welcome to Program HASH Convert!!! --|")
+            print("           Dev by BabyH@ck V.4.2.7\n")
+            print("[1] MD5 HASH")
+            print("[2] SHA1 HASH")
+            print("[3] SHA256 HASH")
+            print("[4] SHA384 HASH")
+            print("[5] SHA512 HASH")
+            print("\n[99] Advance Mode")
+            print("\n*** Do you want to help menu,Please Enter Help ***")
+            print("*** Do you want to exit menu,Please Ctrl+c for exit ***")
+            print("-----------------------------------------------------------")
+            menu = str(input("[+] Select menu : "))
+            if menu in ['1', '2', '3', '4', '5','6']:
+                os.system('cls')  
+                main(menu)  
+                
+            elif menu == 'help' or menu == 'Help':
+                os.system('cls')
+                help_menu()
+                os.system('pause')
+               
+            elif menu == '99':
+                os.system('cls')
+                advance()
+                
+            else:
+                os.system('cls')  
+                print("[!] Select menu again")
+
     except KeyboardInterrupt:
-        print("\nExit to program")"""
+        os.system('cls')
+        print("\n[!] Exit the program")
+        print("-------------------------------------")
+        print("Do you want to Support me? You can sent some donate to me hahaha at a below this message")
+        pay = pyqrcode.create(content="I need Hee")
+        print(pay.terminal(module_color='white', background='black'))  
+        os.system('pause')
